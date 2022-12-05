de956b3e9bede68ddcb43659a96a5c8f19d1bb7f
Mohamad Alfian Ramadhan <56055855+nspbdz@users.noreply.github.com>
2022-12-05 11:35:11 +0700

    first

---
+++ main.py
@@ -0,0 +1,21 @@
+# # print("Halo dunia!")
+# import pandas as pd
+# # df = pd.read_excel("data.xlsx")
+# dfs = pd.read_excel("data.xlsx", sheet_name=None)
+# # df = pd.read_csv('data.xlsx')
+# # In [2]: titanic = pd.read_csv("data/titanic.csv")
+# # df["Duration"] = df["Duration"].replace({60: 10})
+# # df["Pulse"] = df["Pulse"].replace({110: 9}) 
+
+# # print(dfs.head(1))
+
+import pandas as pd
+
+
+
+dfs = pd.read_excel("data.xlsx", sheet_name=None)
+ 
+
+# df = pd.DataFrame(dict)
+
+print(dfs)
